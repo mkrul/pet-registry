9b9cebc11ed0d4cbf67c40432195e03cc2648b13
Misha Krul <mishakrul@Mishas-MacBook-Pro.local>
2024-10-02 07:05:28 -0400

    search is broken

--- app/services/reports/search.rb
+++ app/services/reports/search.rb
@@ -15,18 +15,7 @@ class Reports::Search < ActiveInteraction::Base
 
   def search_options
     {
-      fields: [
-        { title: 3 },    # Boost matches in title by 3x
-        { species: 2 },  # Boost matches in species by 2x
-        { description: 1 },
-        { name: 1 },
-        { gender: 1 },
-        { breed_1: 1 },
-        { breed_2: 1 },
-        { color_1: 1 },
-        { color_2: 1 },
-        { color_3: 1 }
-      ],
+      fields: %w[title description name gender species breed_1 breed_2 color_1 color_2 color_3],
       misspellings: { edit_distance: 2 },
       operator: 'or',
       where: { status: 'active' },
