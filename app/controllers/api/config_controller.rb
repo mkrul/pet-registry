4f7d18a0813340fd7e5a8738d8ad0629d5fa8093
Misha Krul <mishakrul@Mishas-MacBook-Pro.local>
2024-12-02 08:12:44 -0500

    remove oauth

--- app/controllers/api/config_controller.rb
+++
@@ -1,9 +0,0 @@
-# app/controllers/api/config_controller.rb
-
-module Api
-  class ConfigController < ApplicationController
-    def google_client_id
-      render json: { client_id: Rails.application.credentials.google_oauth[:client_id] }, status: :ok
-    end
-  end
-end
--- app/src/components/common/AppRouter.tsx
+++ app/src/components/common/AppRouter.tsx
@@ -6,7 +6,6 @@ import ReportShowPage from "../../pages/reports/ReportShowPage";
 import Footer from "../shared/Footer";
 import PrivateRoute from "./PrivateRoute";
 import LoginPage from "../../pages/auth/LoginPage";
-import OAuthHandler from "../auth/OAuthHandler";
 import ScrollToTop from "./ScrollToTop";
 import SignUpPage from "../../pages/auth/SignUpPage";
 
@@ -16,9 +15,6 @@ const AppRouter = () => {
       <NavBar />
       <ScrollToTop />
       <Routes>
-        {/* OAuth Callback Route */}
-        <Route path="/auth/callback" element={<OAuthHandler />} />
-
         {/* Public Routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<ReportIndexPage />} />
