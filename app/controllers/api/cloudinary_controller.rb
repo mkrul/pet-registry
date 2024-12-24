8b188edb5749cb7c6d34502a8aa24cf18dcdac57
Misha Krul <mishakrul@Mishas-MacBook-Pro.local>
2024-12-24 06:39:58 -0500

    KAN-74 remove cloudinary controller

--- app/controllers/api/cloudinary_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::CloudinaryController < ApplicationController
-  def credentials
-    render json: {
-      cloud_name: Rails.application.credentials.dig(:cloudinary, Rails.env.to_sym, :cloud_name),
-      api_key: Rails.application.credentials.dig(:cloudinary, Rails.env.to_sym, :api_key)
-    }, status: :ok
-  end
-end
