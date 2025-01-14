0db001a16356b1d85a0420a52fe0b85a2a4a9c8d
Misha Krul <mishakrul@Mishas-MacBook-Pro.local>
2025-01-14 03:51:52 -0500

    add production domain info to creds

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -3,7 +3,14 @@
 if Rails.env.development?
   require 'byebug/core'
   begin
-    Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
+    if Rails.env.development?
+      Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
+    else
+      protocol = Rails.application.credentials.dig(:domain, Rails.env.to_sym, :protocol)
+      top_level = Rails.application.credentials.dig(:domain, Rails.env.to_sym, :top_level)
+      second_level = Rails.application.credentials.dig(:domain, Rails.env.to_sym, :second_level)
+      Byebug.start_server "#{protocol}://#{second_level}.#{top_level}", ENV.fetch('BYEBUG_SERVER_PORT', 8989).to_i
+    end
   rescue Errno::EADDRINUSE
     Rails.logger.debug 'Byebug server already running'
   end
