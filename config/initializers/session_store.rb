496be021b3e793b90fde9774954ec75971b8ee54
Misha Krul <mishakrul@Mishas-MacBook-Pro.local>
2025-01-15 12:40:16 -0500

    update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.session_store :cookie_store,
   key: '_pet_registry_session',
-  domain: Rails.env.production? ? '.herokuapp.com' : :all,
+  domain: Rails.application.credentials.dig(:domain, Rails.env.to_sym, :url),
   same_site: :lax,
   secure: Rails.env.production?,
   httponly: true
