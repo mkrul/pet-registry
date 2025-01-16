5cc4d1e24ff6bf34ce993b2fb0c2f34a5799b1cb
Misha Krul <mishakrul@Mishas-MacBook-Pro.local>
2025-01-16 05:32:10 -0500

    comment out elasticsearch config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,8 +1,3 @@
-if Rails.env.production? && ENV['BONSAI_URL'].present?
-  Elasticsearch::Client.new(
-    url: ENV['BONSAI_URL'],
-    transport_options: {
-      headers: { 'x-elastic-product' => 'true' }
-    }
-  )
-end
+# if Rails.env.production? && ENV['BONSAI_URL'].present?
+#   Elasticsearch::Client.new(url: ENV['BONSAI_URL'])
+# end
--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,12 +1,3 @@
 if Rails.env.production? && ENV['BONSAI_URL'].present?
-  Searchkick.client = Elasticsearch::Client.new(
-    url: ENV['BONSAI_URL'],
-    transport_options: {
-      request: {
-        headers: {
-          'x-elastic-product' => 'true'
-        }
-      }
-    }
-  )
+  Searchkick.client = Elasticsearch::Client.new(url: ENV['BONSAI_URL'])
 end
