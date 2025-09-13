c1f96aaf7b3027de81b8c326b7bd7492dc1d5de2
Misha Krul <mishakrul@mishas-macbook-pro.taildba5.ts.net>
2025-09-13 06:16:06 -0400

    remove images from cursorignore

--- config/initializers/active_storage_cloudinary_folders.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# This file is no longer needed as we're using the proper Rails approach
-# with multiple Cloudinary service configurations in storage.yml
-# The cloudinary_reports service automatically handles the folder structure
